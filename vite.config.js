97d848c5bcec0361a01bfbdd6a52d70ace457b7d
Shashank Nath <shashanknath@Shashanks-MacBook-Air.local>
2025-08-06 17:30:45 +0530

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,
