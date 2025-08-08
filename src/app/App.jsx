613dea4035923704835db8eed194758a1e6e718a
Shashank Nath <shashanknath@Shashanks-MacBook-Air.local>
2025-08-08 18:38:29 +0530

    Added new Resume

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,7 +8,7 @@ import InputField from "../common/components/InputField/InputField";
 import TextAreaField from "../common/components/TextAreaField/TextAreaField";
 import SubmitButton from "../common/components/SubmitButton/SubmitButton";
 import Loader from "../common/components/Loader/Loader";
-import cv from "../assets/files/LatestCV.pdf";
+import cv from "../assets/files/Shashank-Nath-Resume.pdf";
 
 // import icons
 import { FaReact } from "react-icons/fa";
